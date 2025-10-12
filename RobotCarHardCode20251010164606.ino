929a766e7a76249d493b7e9d261f45a982fbae56
HiruKub <160151957+HiruKub@users.noreply.github.com>
2025-10-12 16:12:28 +0700

    Update RobotCarHardCode20251010164606.ino

--- RobotCarHardCode20251010164606.ino
+++ RobotCarHardCode20251010164606.ino
@@ -1,7 +1,9 @@
+#include <NewPing.h>
 
-
+#define MAX_DISTANCE 200
 #define echo 12
 #define trig 13
+NewPing sonar(trig, echo, MAX_DISTANCE);
 
 #define speakerPin 3
 
@@ -66,6 +68,7 @@ Cordinate position = { startX, startY, North };
 int caseNum = 1;
 
 void setup() {
+  lowBreak();
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -80,9 +83,10 @@ void setup() {
   pinMode(echo, INPUT);   //สั่งให้ขา echo ใช้งานเป็น input
   pinMode(trig, OUTPUT);  //สั่งให้ขา trig ใช้งานเป็น output
   Serial.begin(9600);
-
-  // caseNum = 1;
-  // caseDefine();
+  pinMode(speakerPin, OUTPUT);
+  caseNum = 1;
+  caseDefine();
+  // pushBoxforOneCell();
 }
 
 void loop() {
@@ -99,21 +103,33 @@ void loop() {
   // } else {
   //   Forward(leftSpeed, rightSpeed);
   // }
-  followOneCell();
-  Beepbeep(1);
-  turnLeft();
 
-  readSensor();
-  for (int i = 0; i < 5; i++) {
-    Serial.print(sensorValues[i]);
-    Serial.print("\t");
-  }
-  Serial.println("");
-  delay(200);
+  // Beepbeep(1);
+
+  // readSensor();
+  // for (int i = 0; i < 5; i++) {
+  //   Serial.print(sensorValues[i]);
+  //   Serial.print("\t");
+  // }
+  // Serial.println("");
+  // delay(200);
+  // if (RobotOnCross() == 1){
+  //   Serial.println("onCross");
+  //   Beepbeep(1);
+  // }
+
+  // ultraSonic();
 
   // ForwardRight(100);
   // ForwardLeft(100);
   // delay(500);
+
+  // moveOneCell();
+  // turnRight();
+  // moveOneCell();
+  // turnLeft();
+  
+  // ultraSonic();
 }
 
 
@@ -122,75 +138,79 @@ void loop() {
 void caseDefine() {
   switch (caseNum) {
     case 1:  // s r s s s l s s s r s s l s no box
+      lowBreak();
       if (ultraSonic() > 2 && ultraSonic() < 30) {
         turnRight();
-        followOneCell();
+        moveOneCell();
         turnLeft();
-        followOneCell();
+        moveOneCell();
         turnRight();
+        moveOneCell();
       } else {
-        followOneCell();
+        moveOneCell();
         turnRight();
-        followOneCell();
+        moveOneCell();
       }
-      followOneCell();
-      followOneCell();
+      moveOneCell();
+      moveOneCell();
       turnLeft();
-      followOneCell();
-      followOneCell();
-      followOneCell();
+      moveOneCell();
+      moveOneCell();
+      moveOneCell();
       turnRight();
-      followOneCell();
+      moveOneCell();
       if (ultraSonic() > 2 && ultraSonic() < 30) {
         turnLeft();
-        followOneCell();
+        moveOneCell();
         turnRight();
-        followOneCell();
+        moveOneCell();
         lowBreak();
         Beepbeep(2);
         delay(1000);
         caseNum = 11;
         caseDefine();
+
       } else {
-        followOneCell();
+        moveOneCell();
         turnLeft();
-        followOneCell();
+        moveOneCell();
         lowBreak();
         Beepbeep(2);
         delay(1000);
         caseNum = 10;
         caseDefine();
+
       }
       break;
     case 10:  //back to start 1 no box:a s r s s s l push s s s r s s s l s
       turnAround();
-      followOneCell();
+      moveOneCell();
       turnRight();
-      followOneCell();
-      followOneCell();
-      followOneCell();
+      moveOneCell();
+      moveOneCell();
+      moveOneCell();
       turnLeft();
       pushOrangebox();
-      followOneCell();
-      followOneCell();
-      followOneCell();
+      moveOneCell();
+      moveOneCell();
+      moveOneCell();
       turnRight();
-      followOneCell();
-      followOneCell();
+      moveOneCell();
+      moveOneCell();
       turnLeft();
       if (ultraSonic() > 2 && ultraSonic() < 30) {
         turnRight();
-        followOneCell();
+        moveOneCell();
         turnLeft();
-        followOneCell();
+        moveOneCell();
         lowBreak();
         Beepbeep(3);
         delay(1000);
         break;
       } else {
-        followOneCell();
+        moveOneCell();
         turnRight();
-        followOneCell();
+        moveOneCell();
         lowBreak();
         Beepbeep(3);
         delay(1000);
@@ -198,34 +218,34 @@ void caseDefine() {
       }
     case 11:
       turnAround();
-      followOneCell();
+      moveOneCell();
       turnLeft();
-      followOneCell();
+      moveOneCell();
       turnRight();
-      followOneCell();
-      followOneCell();
+      moveOneCell();
+      moveOneCell();
       turnLeft();
       pushOrangebox();
-      followOneCell();
-      followOneCell();
-      followOneCell();
+      moveOneCell();
+      moveOneCell();
+      moveOneCell();
       turnRight();
-      followOneCell();
-      followOneCell();
+      moveOneCell();
+      moveOneCell();
       turnLeft();
       if (ultraSonic() > 2 && ultraSonic() < 30) {
         turnRight();
-        followOneCell();
+        moveOneCell();
         turnLeft();
-        followOneCell();
+        moveOneCell();
         lowBreak();
         Beepbeep(3);
         delay(1000);
         break;
       } else {
-        followOneCell();
+        moveOneCell();
         turnRight();
-        followOneCell();
+        moveOneCell();
         lowBreak();
         Beepbeep(3);
         delay(1000);
@@ -236,46 +256,56 @@ void caseDefine() {
 }
 
 void pushOrangebox() {  //s l s / r s s / r s s / r s r / s s r / s l s / l s s / l s r / s r s / r
-  followOneCell();      //push
+  pushBoxforOneCell();      //push
   turnLeft();
-  followOneCell();
+  moveOneCell();
 
   turnRight();
-  followOneCell();
-  followOneCell();
+  moveOneCell();
+  moveOneCell();
 
   turnRight();
-  followOneCell();
-  followOneCell();
+  moveOneCell();
+  moveOneCell();
 
   turnRight();
-  followOneCell();
+  moveOneCell();
   turnRight();
 
-  followOneCell();  //push
-  // followOneCell();
+  pushBoxforOneCell();  //push
+  // moveOneCell();
   turnRight();
 
-  followOneCell();
+  moveOneCell();
   turnLeft();
-  followOneCell();
-
-  turnLeft();
-  followOneCell();
-  followOneCell();
+  moveOneCell();
 
   turnLeft();
-  followOneCell();
+  pushBoxforOneCell();
+  pushBoxforOneCell();
+
+  turnLeft();
+  moveOneCell();
   turnRight();
 
-  followOneCell();
+  moveOneCell();
   turnRight();
-  followOneCell();
+  pushBoxforOneCell();
 
   turnRight();
 }
-
-void followOneCell() {
+void pushBoxforOneCell(){
+  moveOneCell();
+  Forward(200, 180);
+  delay(600);
+  lowBreak(); 
+  delay(60);
+  Backward(leftSpeed, rightSpeed);
+  delay(500);
+  lowBreak();
+  delay(60);
+}
+void moveOneCell() {
   // leave the current node slightly so we don't retrigger immediately
   // while (millis() - t0 < 700) {
   //   Forward(leftSpeed, rightSpeed);
@@ -289,54 +319,46 @@ void followOneCell() {
     Forward(leftSpeed, rightSpeed);
   }
   changePosition();
+  Serial.println("walking 1 cell");
   highBreak();
-  delay(500);
+  delay(120);
 }
 
 
 int ultraSonic() {
-  digitalWrite(trig, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(trig, LOW);  //ใช้งานขา trig
-
-  duration = pulseIn(echo, HIGH);  //อ่านค่าของ echo เป็น microseconds แล้วนำมาหารให้เป็น centimeters
-  distance = duration / 48.25;     //คำนวณหาค่า centimeters
-  Serial.print(distance);          //แสดงค่าระยะเป็น centimeters
-  Serial.print(" cm\n");
+  int distance = sonar.ping_cm();
+  Serial.println(distance);
   return distance;
 }
 
 int RobotOnCross() {
   static unsigned long lastTime = 0;
   unsigned long now = millis();
-  if (now - lastTime < 300) return 0;  //FIX THIS
+  if (now - lastTime < 300) return 0;
 
   readSensor();
   int count = 0;
   for (int i = 0; i < 5; i++) {
-    if (sensorValues[i] > blackValue) {
-      sensorBlack[i] = 1;
-    } else {
-      sensorBlack[i] = 0;
-    }
-    if (sensorBlack[i] == 1) {
+    sensorBlack[i] = (sensorValues[i] > blackValue) ? 1 : 0;
+    if (sensorBlack[i]){
       count++;
-    }
+    } 
   }
+
   if (count >= 3) {
+    lastTime = now;
     return 1;
-  } else return 0;
+  }
+  return 0;
 }
 
 void Beepbeep(int Count) {
   for (int i = 0; i < Count; i++) {
-    tone(speakerPin, 1000, 1000);
-    noTone(speakerPin);
+    digitalWrite(speakerPin, HIGH);
+    delay(400);
+    digitalWrite(speakerPin, LOW);
     delay(400);
   }
-  noTone(speakerPin);
 }
 
 void Forward(int SpeedL, int SpeedR) {
@@ -355,15 +377,15 @@ void Forward(int SpeedL, int SpeedR) {
   if (weight > 0) {
     ForwardRight(SpeedR);
     ForwardLeft(SpeedL - 50);
-    Serial.println("Turn Right");
+    // Serial.println("Turn Right");
   } else if (weight < 0) {
     ForwardRight(SpeedR - 50);
     ForwardLeft(SpeedL);
-    Serial.println("Turn Left");
+    // Serial.println("Turn Left");
   } else {
     ForwardRight(SpeedR);
     ForwardLeft(SpeedL);
-    Serial.println("Go Forward");
+    // Serial.println("Go Forward");
   }
 }
 
@@ -377,9 +399,9 @@ void snapToLine() {  //ใส่หลังจาก Turn
     int b3 = (sensorValues[3] > blackValue);
     int b4 = (sensorValues[4] > blackValue);
     if (b2 && (b1 == b3)) break;
-    Forward(110, 110);
+    Forward(140, 140);
   }
-  Forward(120, 120);
+  Forward(150, 150);
   delay(100);
   lowBreak();
 }
@@ -388,18 +410,19 @@ void turnLeft() {
   position.direction = (position.direction + 3) % 4;
   BackwardLeft(140);
   ForwardRight(120);
+  delay(120);
 
   unsigned long t0 = millis();
   while (millis() - t0 < 800) {
     readSensor();
-    if (sensorValues[2] < blackValue) break;  // go
+    if (sensorValues[2] < blackValue) break;  // go away from line
   }
 
   bool seenOn = false;
   t0 = millis();
   while (millis() - t0 < 1200) {
     readSensor();
-    bool centerOn = (sensorValues[2] > blackValue);
+    bool centerOn = (sensorValues[2] > blackValue);// find the line
     if (centerOn) {
       seenOn = true;
       break;
@@ -407,24 +430,26 @@ void turnLeft() {
   }
   lowBreak();
   snapToLine();
+  Serial.println("TurnLeft");
 }
 
 void turnRight() {
   position.direction = (position.direction + 1) % 4;
-  ForwardLeft(120);
-  BackwardRight(100);
+  ForwardLeft(140);
+  BackwardRight(120);
+  delay(120);
 
   unsigned long t0 = millis();
   while (millis() - t0 < 800) {
     readSensor();
-    if (sensorValues[2] < blackValue) break;  // go
+    if (sensorValues[2] < blackValue) break;  // go away from line
   }
 
   bool seenOn = false;
   t0 = millis();
   while (millis() - t0 < 1200) {
     readSensor();
-    bool centerOn = (sensorValues[2] > blackValue);
+    bool centerOn = (sensorValues[2] > blackValue); // find the line
     if (centerOn) {
       seenOn = true;
       break;
@@ -432,12 +457,14 @@ void turnRight() {
   }
   lowBreak();
   snapToLine();
+  Serial.println("TurnRight");
 }
 
 void turnAround() {
   position.direction = (position.direction + 2) % 4;
-  ForwardLeft(150);
-  BackwardRight(130);
+  ForwardLeft(180);
+  BackwardRight(160);
+  delay(120);
   unsigned long t0 = millis();
   while (millis() - t0 < 800) {
     readSensor();
@@ -465,6 +492,33 @@ void turnAround() {
   snapToLine();  // fine-align on the corridor
 }
 
+void Backward(int SpeedL, int SpeedR) {
+    readSensor();
+  Serial.println("This worked naja");
+  int weight = 0;
+  for (int i = 0; i < 5; i++) {
+    sensorBlack[i] = (sensorValues[i] > blackValue) ? 1 : 0;
+  }
+  for (int i = 0; i < 5; i++) {
+    if (sensorBlack[i] > 0) {
+      weight += (i * 1000) - 2000;
+    }
+    sensorBlack[i] = 0;
+  }
+  if (weight > 0) {
+    BackwardRight(SpeedR);
+    BackwardLeft(SpeedL - 50);
+    Serial.println("Turn Right");
+  } else if (weight < 0) {
+    BackwardRight(SpeedR - 50);
+    BackwardLeft(SpeedL);
+    Serial.println("Turn Left");
+  } else {
+    BackwardRight(SpeedR);
+    BackwardLeft(SpeedL);
+    Serial.println("Go Forward");
+  }
+}
 
 void changePosition() {
   if (position.direction == North) position.y++;
@@ -473,6 +527,54 @@ void changePosition() {
   else if (position.direction == West) position.x--;
 }
 
+
+void readSensor() {
+  sensorValues[0] = analogRead(S1);
+  sensorValues[1] = analogRead(S2);
+  sensorValues[2] = analogRead(S3);
+  sensorValues[3] = analogRead(S4);
+  sensorValues[4] = analogRead(S5);
+}
+
+void ForwardRight(int Speed) {
+  //motor 1
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enA, Speed);
+}
+
+void BackwardRight(int Speed) {
+  //motor 1
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, Speed);
+}
+
+void BackwardLeft(int Speed) {
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enB, Speed);
+}
+
+void ForwardLeft(int Speed) {
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enB, Speed);
+}
+
+void lowBreak() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+void highBreak() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, HIGH);
+}  
 // void showGridInt(const int grid[ROWS][COLS]) {
 //   for (int y = 0; y < ROWS; y++) {
 //     for (int x = 0; x < COLS; x++) {
@@ -562,60 +664,8 @@ void changePosition() {
 //     else if (diff == 3) turnLeft();
 //     else if (diff == 2) turnAround();
 
-//     followOneCell();
+//     moveOneCell();
 //   }
 //   Serial.println("Yippee");
 //   lowBreak();
 // }
-
-void Backward(int SpeedL, int SpeedR) {
-  BackwardLeft(SpeedL);
-  BackwardRight(SpeedR);
-}
-void readSensor() {
-  sensorValues[0] = analogRead(S1);
-  sensorValues[1] = analogRead(S2);
-  sensorValues[2] = analogRead(S3);
-  sensorValues[3] = analogRead(S4);
-  sensorValues[4] = analogRead(S5);
-}
-
-void ForwardRight(int Speed) {
-  //motor 1
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  analogWrite(enA, Speed);
-}
-
-void BackwardRight(int Speed) {
-  //motor 1
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  analogWrite(enA, Speed);
-}
-
-void BackwardLeft(int Speed) {
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  analogWrite(enB, Speed);
-}
-
-void ForwardLeft(int Speed) {
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  analogWrite(enB, Speed);
-}
-
-void lowBreak() {
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-}
-
-void highBreak() {
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, HIGH);
-}
